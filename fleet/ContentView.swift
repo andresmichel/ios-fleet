04e300afc74ac66ee27399839b0b14b29f0bb217
andresmichel <andresmichelg@gmail.com>
2020-04-23 18:41:30 -0700

    add mapbox view

--- fleet/ContentView.swift
+++ fleet/ContentView.swift
@@ -5,12 +5,28 @@
 //  Created by Andres Michel on 4/7/20.
 //  Copyright © 2020 Andres Michel. All rights reserved.
 //
+//
+//import SwiftUI
+//
+//struct ContentView: View {
+//    var body: some View {
+//        Text("Hello, World!")
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
 
 import SwiftUI
+import Mapbox
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        MapView().centerCoordinate(.init(latitude: 37.791293, longitude: -122.396324)).zoomLevel(16)
     }
 }
 
